c4b182c7da3253f2e3a5c7fdc6c6b5c232243599
Eric Conlon <econlon@twitter.com>
2016-02-07 11:03:29 -0800

    mo

--- src/main/scala/abts2/ABT.scala
+++ src/main/scala/abts2/ABT.scala
@@ -4,14 +4,29 @@ import prelude._
 
 // https://github.com/psygnisfive/SimpleFP-v2/blob/master/src/Utils/ABT.hs
 
-sealed trait Variable
+sealed trait Variable {
+  import Variable.{Free, Bound, Meta}
+
+  def name: String
+  def equals(other: Variable): Boolean =
+    (this, other) match {
+      case (Free(x), Free(y)) => x == y
+      case (Bound(_, i), Bound(_, j)) => i == j
+      case (Meta(i), Meta(j)) => i == j
+      case _ => false
+    }
+}
+
 object Variable {
   case class Free(name: String) extends Variable
-  case class Bound(index: Int) extends Variable
-  //case class Meta(number: Int) extends Variable
+  case class Bound(name: String, index: Int) extends Variable
+  case class Meta(number: Int) extends Variable {
+    override def name = "?" + number
+  }
 }
 
 sealed trait ABT[F[_]]
+
 object ABT {
   case class Var[F[_]](variable: Variable) extends ABT[F]
   case class In[F[_]](term: F[Scope[F]]) extends ABT[F]
